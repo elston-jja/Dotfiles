5809377e68bddbc3a2d7bbdd9aef716fe9d6cc2f
elston <epeglab@gmail.com>
2016-07-01 15:50:32 -0400

    added new revision of tic-tac-toe, and added AI example(do not follow)

--- tic-tac-toe/tic-tac-toe.py
+++ tic-tac-toe/tic-tac-toe.py
@@ -1,86 +1,209 @@
-import pygame
-import string
-def mouse_quadrant():
-    if event.type == pygame.MOUSEBUTTONUP:
-        mouse = pygame.mouse.get_pos()
-        allchars = ''.join(chr(i) for i in xrange(256))
-        identity = string.maketrans('', '')
-        nondigits = allchars.translate(identity, string.digits)
-        mouse_x = ((str(mouse)[0:4]).translate(identity, nondigits))
-        mouse_y = ((str(mouse)[5:]).translate(identity, nondigits))
-        mouse_y = int(mouse_y)
-        mouse_x = int(mouse_x)
-        print mouse_x,mouse_y
-        if (mouse_x < 170) and (mouse_y < 170) :
-            print "quad 1"
-            return 1
-        elif (170 < mouse_x < 340) and (mouse_y < 170 ):
-            print "quad 2"
-            return 2
-        elif (340 < mouse_x) and (mouse_y < 170):
-            print "quad 3"
-            return 3
-        elif (mouse_x < 170) and (170 < mouse_y < 340 ):
-            print "quad 4"
-            return 4
-        elif (170 < mouse_x < 340) and (170 < mouse_y < 340 ):
-            print "quad 5"
-            return 5
-        elif (340 < mouse_x) and (170 < mouse_y < 340 ):
-            print "quad 6"
-            return 6
-        elif (mouse_x < 170) and (340 < mouse_y) :
-            print "quad 7"
-            return 7
-        elif (170 < mouse_x < 340) and (340 < mouse_y):
-            print "quad 8"
-            return 8
+import pygame,os
+global done
+
+class Box(pygame.sprite.Sprite):
+
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.width = 170
+        self.height = 170
+        self.image = pygame.Surface([self.width,self.height])
+        self.image.fill((255,255,255))
+        self.image.set_colorkey((255,255,255))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        all_sprites_group.add(self)
+
+    def get_mouse_pos(self):
+        self.mouse = pygame.mouse.get_pos()
+
+        self.mouse_x = self.mouse[0]
+        self.mouse_y = self.mouse[1]
+        
+        if self.rect.x < self.mouse_x < self.rect.right:
+            if self.rect.y < self.mouse_y < self.rect.bottom:
+
+                block_exists = self.check_block(self.rect.x,self.rect.y)
+                if block_exists:
+                    pass
+                else:
+                    x_turn = game.current_turn()
+                    if x_turn:
+                        piece = X_piece(self.rect.x,self.rect.y)
+                        game.x_turn = not game.x_turn
+                    else:
+                        piece = O_piece(self.rect.x,self.rect.y)
+                        game.x_turn = not game.x_turn
+
+    def check_block(self, x, y):
+        for piece in pieces_group:
+            if piece.rect.x == x:
+                if piece.rect.y == y:
+                    return True
+
+    def update(self):
+        pass
+        
+
+class Piece(pygame.sprite.Sprite):
+
+    def __init__(self,x,y):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.x = x
+        self.y = y
+        self.width = 170
+        self.height = 170
+        self.image = pygame.Surface([self.width,self.height])
+        self.rect = self.image.get_rect()
+        self.image.fill((255,255,255))
+        self.image.set_colorkey((255,255,255))
+        self.rect.x = self.x
+        self.rect.y = self.y
+        all_sprites_group.add(self)
+        pieces_group.add(self)
+
+    def get_mouse_pos(self):
+        pass
+
+class X_piece(Piece):
+
+    def __init__(self, x, y):
+        Piece.__init__(self, x, y)
+        self.x_image = pygame.image.load("Images/x.png").convert_alpha()
+        self.name = "x"
+
+
+    def update(self):
+        screen.blit(self.x_image,(self.x + 50,self.y + 50))
+
+class O_piece(Piece):
+
+    def __init__(self, x, y):
+        Piece.__init__(self, x, y)
+        self.o_image = pygame.image.load("Images/o.png").convert_alpha()
+        self.name = "o"
+
+    def update(self):
+        screen.blit(self.o_image,(self.x + 50,self.y + 50))
+
+
+class Game:
+
+    def __init__(self):
+        self.turn_number = 0
+        self.x_turn = True
+        self.done = False
+        
+    def current_turn(self):
+        if self.x_turn:
+            return True
         else:
-            print "quad 9"
-            return 9
-def input_is_valid():
-    try:
-        piece_validate = raw_input()
-        piece_validate = int(piece_validate)
-        if not piece_validate:
-            print "Please enter a character to get started"
-        elif piece_validate > 2 :
-            print "Please choose a valid option"
-        else:
-            return (piece_validate)
-    except:
-        print "Please choose a valid option"
-def intro():
-    print "please choose a character\n1.X\n2.O\nUse the values 1 or 2 to choose: ",
-    #
-def draw_x(mouse):
-    if location ==  1:
-        screen.blit(x_piece,(65,65))
+            return False
+        
+    def check_win(self):
+
+        for block in pieces_group:
+            self.name = block.name
+            firstx = block.rect.x
+            firsty = block.rect.y
+
+            for block in pieces_group:
+                if block.name == self.name:
+
+                    if block.rect.x + 170 == firstx:
+                        if block.rect.y + 170 == firsty:
+
+                            secondx = block.rect.x
+                            secondy = block.rect.y
+                            for block in pieces_group:
+                                if block.name == self.name:
+                                    if block.rect.x + 340 == firstx:
+                                        if block.rect.y + 340 == firsty:
+                                            self.win()
+
+                    elif block.rect.x - 170 == firstx:
+                        if block.rect.y + 170 == firsty:
+
+                            secondx = block.rect.x
+                            secondy = block.rect.y 
+                            for block2 in pieces_group:
+                                if block2.name == self.name:
+                                    if block2.rect.x - 340 == firstx:
+                                        if block2.rect.y + 340 == firsty:
+                                            self.win()
+                                            
+
+                    if block.rect.x == firstx:
+                        if block.rect.y - 170 == firsty:
+                            for block2 in pieces_group:
+                                if block2.name == self.name:
+                                    if block2.rect.x == firstx:
+                                        if block2.rect.y - 340 == firsty:
+                                            self.win()
+                                            
+
+                    elif block.rect.y == firsty:
+                        if block.rect.x - 170 == firstx:
+                            for block2 in pieces_group:
+                                if block2.name == self.name:
+                                    if block2.rect.y == firsty:
+                                        if block2.rect.x - 340 == firstx:
+                                            self.win()
+                                            
+    def win(self):
+
+        print ("Therefore the winner is " + self.name)
+        self.done = True
+        # Not needed, but only prints winner once
+        exit()
+
+
+class Ai():
+    # Removed AI as did not function well
+    pass
+
+                
 pygame.init()
-pygame.display.set_caption("Tic-Tac-Toe")
-screen = pygame.display.set_mode((512,512))
-x_piece = pygame.image.load("Images/x.png").convert_alpha()
-o_piece = pygame.image.load("Images/o.png").convert_alpha()
-background = pygame.image.load("Images/background.jpg").convert()
-quad_one = None
-running = True
-piece = 1
-location = None
-while running is True:
-    # any events that need to be recorded in here
-    #piece = input_is_valid()
-    #when implementing O later
-    if piece == 1:
-        piece = x_piece
-    elif piece == 2:
-        piece = o_piece
-    screen.blit(background,(0,0))
+
+                
+all_sprites_group = pygame.sprite.Group()
+pieces_group = pygame.sprite.Group()
+
+box0 = Box(0,0)
+box1 = Box(170,0)
+box2 = Box(340,0)
+box3 = Box(0,170)
+box4 = Box(170,170)
+box5 = Box(340,170)
+box6 = Box(0,340)
+box7 = Box(170,340)
+box8 = Box(340,340)
+
+game = Game()
+
+size = (512,512)
+
+screen = pygame.display.set_mode(size)
+
+background = pygame.image.load("Images/background.jpg").convert_alpha()
+
+while not game.done:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-        mouse = mouse_quadrant()
-        if mouse:
-            print mouse
-    draw_x(mouse)
-    #screen.blit(x_piece,(65,65))
-    pygame.display.update()
+            game.done = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            for box in all_sprites_group:
+                box.get_mouse_pos()
+                game.check_win()
+
+    screen.blit(background,(0,0))
+    all_sprites_group.draw(screen)
+    all_sprites_group.update()
+    
+    pygame.display.flip()
+pygame.quit()
+
